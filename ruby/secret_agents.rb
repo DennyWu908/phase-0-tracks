2b79dee01a7b78e66f2402da3188cfdc49888ff1
Denny Wu <denny.wu87@gmail.com>
2016-06-27 15:27:15 -0700

    Modify secret agents rb file

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -26,22 +26,23 @@ end
 
 decrypt(encrypt("swordfish"))
 
-=begin
-    Note: The nested method call works because the encrypt method (inside the patentheses) is called first, followed by the decrypt method. Because of this, the word "swordfish" is encrypted and decrypted, and we are able to see the results of using both methods
-=end
+# Note: The nested method call works because of the order in which the method calls are evaluated. The "encrypt" method is called first, then 
+# the "decrypt" method. As such, the characters in the string "swordfish" will be changed according to the code of the "encrypt" method first.
+# After that, the characters of the resulting string ("txpsegjti") will be changed back to "swordfish" using the "decrypt" method. The nested 
+# method call allows us to see the results of using both methods.
 
 begin
-    puts "Please write whether you want to encrypt or decrypt."
-    answer = gets.chomp
-    if answer == "encrypt"
-        puts "Please enter password"
+    puts "Welcome, agent. Please select a function (encrypt or decrypt)."
+    function = gets.chomp
+    if function == "encrypt"
+        puts "Please enter the password you wish to encrypt."
         password = gets.chomp
         puts encrypt(password)
-    elsif answer == "decrypt"
-        puts "Please enter password"
+    elsif function == "decrypt"
+        puts "Please enter the password you wish to decrypt."
         password = gets.chomp
         puts decrypt(password)
     else
-        puts "Please enter a valid response"
+        puts "Please enter a valid response."
     end
-end until answer.downcase == "encrypt" || answer.downcase == "decrypt"
+end until function.downcase == "encrypt" || function.downcase == "decrypt"
