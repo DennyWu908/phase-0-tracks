77428dd2dfe3fb52372e85a5630cfcd801e0bc39
Denny Wu <denny.wu87@gmail.com>
2016-06-28 23:20:01 -0700

    Modify method detective file

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,57 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+ "iNvEsTiGaTiOn".swapcase
+ "iNvEsTiGaTiOn".swapcase!
+ => “InVeStIgAtIoN”
+ # Below is an example of calling these methods, and the result.
+ "HeLlO".swapcase
+ "HeLlO".swapcase!
+ => "hElLo"
 
-# "zom".<???>
-# => “zoom”
+ "zom".insert(index, other_str)
+ "zom".insert(1, 'o')
+ => “zoom”
+ "desert".insert(2, 's')
+ => “dessert”
 
-# "enhance".<???>
-# => "    enhance    "
+ "enhance".center(width, padstr=' ')
+ "enhance".center(15)
+ => "    enhance    "
+ "addition".center(15)
+ => "   addition    "
+ "addition".center(15, 'plus')
+ => "pluadditionplus"
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+ "Stop! You’re under arrest!".upcase
+ "Stop! You’re under arrest!".upcase!
+ => "STOP! YOU’RE UNDER ARREST!"
+ "Good Morning!".upcase
+ "Good Morning!".upcase!
+ => "GOOD MORNING!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+ "the usual" << integer
+ "the usual" << obj
+ "the usual" << " suspects"
+ => "the usual suspects"
+ "you're a" << " wizard, harry"
+ => "you're a wizard, harry"
 
-# " suspects".<???>
-# => "the usual suspects"
+ " suspects".<???>
+ => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+ "The case of the disappearing last letter".<???>
+ => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+ "The mystery of the missing first letter".<???>
+ => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+ "Elementary,    my   dear        Watson!".<???>
+ => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+ "z".<???>
+ => 122 
+ (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+ "How many times does the letter 'a' appear in this string?".<???>
+ => 4
