db48f0af21d27e2c64b1cdee5ae1cfe6481be357
Denny Wu <denny.wu87@gmail.com>
2016-07-02 17:16:20 -0700

    Modify alias manager rb file

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+# Allow a user to input a name, and split the name into an array of two words.
+
+agent = gets.chomp.split(' ')
+
+# Reverse the order of the words, then split them into letters.
+
+agent.rotate(1)
+
+# Convert the array into a string.
+
+# Change each vowel to the next vowel in the alphabetical order.
+
+# Change is consonant to the next consonant in the alphabetical order.
+
+# Print the new name.
+
+# Note: I was unable to write a method using the algorithm described. I have decided to complete the assignment based on the alternate set of
+# instructions. The method below will convert each letter in a name to the next letter in alphabetical order.
+# Below is the psuedocode for this method, along with the actual code.
+
