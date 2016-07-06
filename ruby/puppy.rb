6aedcf22facc400daf5a4670dd14b42b942835af
Denny Wu <denny.wu87@gmail.com>
2016-07-05 18:11:35 -0700

    Modify puppy rb file

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+SPECIES ------------------------
+Golden Retriever Puppy
+
+CHARACTERISTICS ----------------
+Eyelash length: long
+Friendly: very
+Eye count: 2
+Name: depends on owner
+Color: gold
+
+BEHAVIOR -----------------------
+Run
+Sit in Lap
+Fetch
+
