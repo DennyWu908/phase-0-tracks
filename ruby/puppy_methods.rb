b04c1c204b86b869445c7c9976cdb47a89805c46
Denny Wu <denny.wu87@gmail.com>
2016-07-05 21:59:04 -0700

    Modify puppy methods rb file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,25 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(years)
+  	puts years*7
+  end
+
+  def call(name)
+  	puts "#{name.upcase}! HERE, #{name.upcase}!"
+  end
+
 end
+
+rover = Puppy.new
+
+rover.fetch("frisbee")
 
