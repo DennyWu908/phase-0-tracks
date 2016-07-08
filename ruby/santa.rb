2258309048c7299e997975330a868603e155ce3e
Denny Wu <denny.wu87@gmail.com>
2016-07-08 16:33:48 -0700

    Modify santa rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,13 +26,31 @@ class Santa
 		@ethnicity
 	end
 
+	def reindeer_ranking
+	@reindeer_ranking
+	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at
+		@reindeer_ranking.rotate(4)
+	end
+
 end
 
-nick = Santa.new("male","caucasian")
+nick = Santa.new("male","caucasian","english","other")
 
 nick.speak
 
 nick.eat_milk_and_cookies("sugar cookie")
+
+puts "This Santa is #{nick.celebrate_birthday} years old."
+
+puts "This Santa is of #{nick.ethnicity} ethnicity."
+
+puts "This Santa's ranking of his reindeer, from favorite to least favorite, is as follows. #{nick.get_mad_at}."
 
 workshop = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
