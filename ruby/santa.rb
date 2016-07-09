18571db8ca8fe4a68aa6c2a17ead6f1b087733d2
Denny Wu <denny.wu87@gmail.com>
2016-07-08 20:05:08 -0700

    Modify santa rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+	attr_reader :ethnicity
+	attr_accessor :gender
 	
 	def initialize(gender,ethnicity,accent,nationality)
 		puts "Initializing Santa instance..."
@@ -7,7 +9,7 @@ class Santa
 		@accent = accent
 		@nationality = nationality
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(0..140)
 	end
 
 	def speak
@@ -17,28 +19,26 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-
+	
 	def age
 		@age
 	end
 	
-	def ethnicity
-		@ethnicity
-	end
-
 	def reindeer_ranking
-	@reindeer_ranking
+		@reindeer_ranking
 	end
-
+	
 	def celebrate_birthday
 		@age += 1
 	end
-
+	
 	def get_mad_at
 		@reindeer_ranking.rotate(4)
 	end
 
 end
+
+# This is the driver code for this assignment.
 
 nick = Santa.new("male","caucasian","english","other")
 
@@ -52,6 +52,12 @@ puts "This Santa is of #{nick.ethnicity} ethnicity."
 
 puts "This Santa's ranking of his reindeer, from favorite to least favorite, is as follows. #{nick.get_mad_at}."
 
+nick.gender = "female"
+
+puts "This Santa's gender is #{nick.gender}."
+
+# Adding code for more diverse Santas and storing instances in an array.
+
 workshop = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -63,3 +69,9 @@ example_genders.length.times do |fc|
 end
 
 p workshop
+
+# This is a loop for Release 4.
+
+25.times do |fc|
+  workshop << Santa.new(example_genders[fc], example_ethnicities[fc], accents[fc], nationalities[fc])
+end
