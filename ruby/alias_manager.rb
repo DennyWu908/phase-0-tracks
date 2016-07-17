76c139fc0977fe9a343e6f061060df649772b84e
Denny Wu <denny.wu87@gmail.com>
2016-07-17 02:58:22 -0700

    Updated code for alias manager rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -53,3 +53,39 @@ end
 code_list.each do |fake|
 	puts "For the purposes of coded messages #{agent} will be known as #{fake}."
 end
+
+# I am resubmitting this assignment. I have revised the code for the program above (the name_change method and the user interface).
+# The updated code is below.
+
+$code_list = {}
+
+def name_change(agent)
+	coded_name = agent.split('')
+
+# After the user enters a name, the name will be split into separate letters, which will be placed in an array.
+
+	coded_name.map! { |code| code.next }
+
+# Each letter in the array will then be changed to the letter that follows it in the alphabet.
+	coded_name
+	false_name = coded_name.join('')
+
+# Finally, the letters will be joined back into a new string. This string is the result of the original name being changed by this method.
+
+	$code_list[agent] = false_name
+
+# The above line will store the changed names in an array, which was initialized at the beginning of the method.
+
+end
+
+loop do
+	puts "Welcome, Agent. This is an experimental program designed to convert your name into a string of random characters, which will be used as your alias in a coded message. To use it, please enter your name. When you are finished, type 'quit'."
+	agent = gets.chomp
+			
+	break if agent == 'quit'
+	puts name_change(agent)
+end
+
+$code_list.each do |agent, false_name|
+	puts "For the purposes of coded messages #{agent} will be known as #{false_name}."
+end
