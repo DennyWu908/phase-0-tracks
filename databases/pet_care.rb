8eb65464e904f7c76f86085f640e6eb15864a2cf
Denny Wu <denny.wu87@gmail.com>
2016-07-27 01:15:16 -0700

    Add ruby code to pet care rb file

--- databases/pet_care.rb
+++ databases/pet_care.rb
@@ -21,3 +21,37 @@ SQL
 
 chart.execute(table_creation)
 
+# The following code can be used to add data to one of the table rows.
+
+chart.execute("INSERT INTO pets (name, species, diet, fed) VALUES ('Rover', 'dog', 'kibble', 'true')")
+
+# Write method to add data to rows.
+
+def scheduler(db, name, species, diet, fed)
+	chart.execute("INSERT INTO pets (name, species, diet, fed) VALUES (?, ?, ?, ?)", [name, species, diet, fed])
+end
+
+# Write method to update values in rows.
+
+def edit(id_number)
+	puts "Enter the type of data you wish to update for this pet."
+		if data_type == 'name'
+			puts "What is this pet's name?"
+			new_name = gets.chomp
+			chart.execute(UPDATE pets SET name=new_name WHERE id=id_number;)
+		elsif data_type == 'species'
+			puts "What is this pet's species?"
+			new_species = gets.chomp
+			chart.execute(UPDATE pets SET species=new_species WHERE id=id_number;)
+		elsif data_type == 'diet'
+			puts "What type of diet does this pet need?"
+			new_diet = gets.chomp
+			chart.execute(UPDATE pets SET diet=new_diet WHERE id=id_number;)
+		else
+			puts "Has this pet been fed yet?"
+			new_fed = gets.chomp
+			chart.execute(UPDATE pets SET diet=new_ded WHERE id=id_number;)
+		end
+	chart.execute(SELECT * FROM pets;)
+end
+		
